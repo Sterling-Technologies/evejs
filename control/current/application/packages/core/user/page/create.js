45b5051f91f1a68f762cc6a6f1e5f910f1342f62
destromon <jestadena@yahoo.com>
2014-06-04 12:45:16 +0800

    Form Templates v3.0
    Added require and handlebar to form templates

--- control/current/application/packages/core/user/page/create.js
+++ control/current/application/packages/core/user/page/create.js
@@ -43,6 +43,7 @@ define(function() {
 		
 		var self = this,
         forms = [
+        controller.path('config') + '/countries.js',
         'text!' + controller.path('user/template') +  'form/tabs.html',
         'text!' + controller.path('user/template') +  'form/picture.html',
         'text!' + controller.path('user/template') +  'form/basic.html',
@@ -51,7 +52,74 @@ define(function() {
         'text!' + controller.path('user/template') +  'form/required.html',
         'text!' + controller.path('user/template') +  'form/company.html'];
 
-        require(forms, function(tabs, picture, basic, contact, social, required, company) {
+        require(forms, function(countries, tabs, picture, basic, contact, social, required, company) {
+            
+            //load basic form template
+            basic = Handlebars.compile(basic)({});
+
+            //load company form template
+            company = Handlebars.compile(company)({
+                user_company         : 'example company',
+                user_job_title       : 'example job title',
+                user_company_street  : 'example street',
+                user_company_city    : 'example city',
+                user_company_state   : 'example state',
+                //We need an array for country code and country name
+                //lets create an array inside company and store all country data on it
+                country              : countries,
+                user_company_postal  : '4012',
+                user_company_phone   : '(000)-000-0000',
+                company_email        : 'email@example.com'
+            });
+
+            //load contact form template
+            contact = Handlebars.compile(contact)({
+                user_website  : 'www.example.com',
+                user_phone    : '09298888888'
+            });
+            
+            //load picture form template
+            picture = Handlebars.compile(picture)({
+                user_photo : 'example.jpg'
+            });
+
+            //load required form template
+            required = Handlebars.compile(required)({
+                user_name       : 'example username',
+                user_slug       : 'example slug',
+                user_email      : 'email@example.com',
+                user_password   : 'example password',
+                user_confirm    : 'example confirm',
+                errors : {
+                    user_name       : '',
+                    user_slug       : '',
+                    user_email      : '',
+                    user_password   : '',
+                    user_confirm    : ''
+                }
+            });
+
+            //load social form template
+            social = Handlebars.compile(social)({
+                user_facebook : 'user facebook',
+                user_twitter  : 'user twitter',
+                user_google   : 'user google'
+            });
+
+            //load tabs template
+            tabs = Handlebars.compile(tabs)({
+                request_uri : '',
+                path        : 'example/path',
+                id          : 'example_id',
+                tabs        : [{
+                    item : {
+                        icon    : 'example.icon',
+                        label   : 'example label'
+                    }
+                }]
+            });
+
+            //append form templates to page
         	self.data.tabs = Handlebars.compile(tabs);
         	self.data.picture = Handlebars.compile(picture);
         	self.data.basic = Handlebars.compile(basic);
