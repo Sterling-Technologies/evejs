c0e22f34dfb7e11eb9cfffc1f5a1a36ac0247a8c
denezra <den.segunial@gmail.com>
2014-06-05 14:38:26 -0700

    Conversion
    Done converting the all js files in the add folder.

--- api/packages/core/user/action/add/phone.js
+++ api/packages/core/user/action/add/phone.js
@@ -1,60 +1,91 @@
 module.exports = function(controller, request, response) {
-	//1. VALIDATE
-	//if no id was set
-	if(!request.variables[0]) {
-		//setup an error response 
-		response.message = JSON.stringify({ 
-			error: true, 
-			message: 'No ID set' });
-		
-		//trigger that a response has been made
-		controller.trigger('user-action-response', request, response);
-		
-		return;
-	} 
-	//2. SETUP
+	var sequence = controller.eden.load('sequence');
+
 	//change the string into a native object
 	var query = controller.eden
 		.load('string', request.message)
 		.queryToHash().get();
-	
-	//if no query
-	if(JSON.stringify(query) == '{}') {
-		//setup an error response
-		response.message = JSON.stringify({ 
-			error: true, 
-			message: 'No Parameters Defined' });
-			
-		//trigger that a response has been made
-		controller.trigger('user-action-response', request, response);
-		
-		return;
+
+	var c = function() {
+		this.render.call();
+	}, public = c.prototype;
+	/* Loader
+	-------------------------------*/
+	public.__load = c.load = function() {
+		if(!this.__instance) {
+			this.__instance = new c();
+		}
+		return this.__instance;
+	};
+	/* Construct
+	-------------------------------*/
+	public.render = function() {
+		sequence.then(function(next) { this.validate })
+		.then(function(next) { this.setup });
+
+		return this;
 	}
-	
-	//3. TRIGGER
-	controller
-		//when there is an error
-		.once('user-add-phone-error', function(error) {
+	/* Public Methods
+	-------------------------------*/
+	//1. VALIDATE: if no id was set
+	public.validate = function() {
+		if(!request.variables[0]) {
+			//setup an error response 
+			response.message = JSON.stringify({ 
+				error: true, 
+				message: 'No ID set' });
+			
+			//trigger that a response has been made
+			controller.trigger('user-action-response', request, response);
+			
+			return;
+		} 
+
+		//if no query
+		if(JSON.stringify(query) == '{}') {
 			//setup an error response
 			response.message = JSON.stringify({ 
 				error: true, 
-				message: error.message });
-			
+				message: 'No Parameters Defined' });
+				
 			//trigger that a response has been made
 			controller.trigger('user-action-response', request, response);
-		})
-		//when it is successfull
-		.once('user-add-phone-success', function() {
-			//set up a success response
-			response.message = JSON.stringify({ error: false });
 			
-			//trigger that a response has been made
-			controller.trigger('user-action-response', request, response);
-		})
-		//Now call to remove the user
-		.trigger(
-			'user-add-phone', 
-			controller, 
-			request.variables[0], 
-			query);
+			return;
+		}
+	}
+	
+	public.setup = function() {
+		//2. TRIGGER
+		controller
+			//when there is an error
+			.once('user-add-phone-error', function(error) {
+				//setup an error response
+				response.message = JSON.stringify({ 
+					error: true, 
+					message: error.message });
+				
+				//trigger that a response has been made
+				controller.trigger('user-action-response', request, response);
+			})
+			//when it is successfull
+			.once('user-add-phone-success', function() {
+				//set up a success response
+				response.message = JSON.stringify({ error: false });
+				
+				//trigger that a response has been made
+				controller.trigger('user-action-response', request, response);
+			})
+			//Now call to remove the user
+			.trigger(
+				'user-add-phone', 
+				controller, 
+				request.variables[0], 
+				query);
+	}
+	/* Private Methods
+	-------------------------------*/
+	/* Adaptor
+	-------------------------------*/
+	return c.load(); 
 };
