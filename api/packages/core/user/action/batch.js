433e66ea80ed945bab10c824ce5ebefa39af3013
destromon <jestadena@yahoo.com>
2014-06-05 14:40:02 +0800

    Revert "Conversion and Fixing"
    This reverts commit 79ff8ecc593ead8875af2c9f95181257a5e87f61.

--- api/packages/core/user/action/batch.js
+++ api/packages/core/user/action/batch.js
@@ -1,79 +1,52 @@
 module.exports = function(controller, request, response) {
     //Batch file called
     response.batch = true;
-    var sequence = controller.eden.load('sequence');
-    var c = function() {
-        this.render.call();
-    }, public = c.prototype;
-    /* Loader
-    -------------------------------*/
-    public.__load = c.load = function() {
-        if(!this.__instance) {
-            this.__instance = new c();
-        }
-        return this.__instance;
-    };
-    /* Construct
-    -------------------------------*/
-    public.render = function() {
-        sequence.then(function(next) { this.validate })
-        .then(function(next) { this.setup });
+    //Converting request messages to JSON
+    var batch = JSON.parse(request.message);
+    //Loop the all the batch request
+    for(var i = 0; i < batch.length; i++){
+        //Check for the batch method 
+        switch(batch[i].method.toLowerCase()){
+            case 'create':
+                require('./create')(controller, request, response);
+                break;
+            case 'remove':
+                require('./remove')(controller, request, response);
+                break;
+            case 'update':
+                require('./update')(controller, request, response);
+                break;
+            case 'detail':
+                require('./detail')(controller, request, response);
+                break;
+            case 'list':
+                require('./list')(controller, request, response);
+                break;
+            case 'address':
+                require('./address')(controller, request, response);
+                break;
+            case 'phone':
+                require('./phone')(controller, request, response);
+                break;
+            case 'photo':
+                require('./photo')(controller, request, response);
+                break;
 
-        return this;
-    }
-    /* Public Methods
-    -------------------------------*/
-    //1. VALIDATE: if no id was set
-    public.validate = function() {
-        //Converting request messages to JSON
-        var batch = JSON.parse(request.message);
-        //Loop the all the batch request
-        for(var i = 0; i < batch.length; i++){
-            //Check for the batch method 
-            switch(batch[i].method.toLowerCase()){
-                case 'create':
-                    require('./create')(controller, request, response);
-                    break;
-                case 'remove':
-                    require('./remove')(controller, request, response);
-                    break;
-                case 'update':
-                    require('./update')(controller, request, response);
-                    break;
-                case 'detail':
-                    require('./detail')(controller, request, response);
-                    break;
-                case 'list':
-                    require('./list')(controller, request, response);
-                    break;
-                case 'address':
-                    require('./address')(controller, request, response);
-                    break;
-                case 'phone':
-                    require('./phone')(controller, request, response);
-                    break;
-                case 'photo':
-                    require('./photo')(controller, request, response);
-                    break;
-
-            }
         }
     }
-    public.setup = function() {
-        //Array that will contain the request message
-        var results = [];
-        controller.once('user-action-response', function(request, response) {
-            //All results will be push to the array results
-            results.push(response.message);
+    //Array that will contain the request message
+    var results = [];
+    controller.once('user-action-response', function(request, response) {
+        //All results will be push to the array results
+        results.push(response.message);
 
-            //If results is equal to request query
-            if(results.length == request.query.batch.length){
-                //All batch results will be JSON stringify
-                response.message = JSON.stringify({ batch:results })
+        //If results is equal to request query
+        if(results.length == request.query.batch.length){
+            //All batch results will be JSON stringify
+            response.message = JSON.stringify({ batch:results })
 
-                //Been trigger to the user action
-                controller.trigger('user-action-response', request, response);
-            }
-        });
-    }
+            //Been trigger to the user action
+            controller.trigger('user-action-response', request, response);
+        }
+    });
 }
--- api/packages/core/user/action/detail.js
+++ api/packages/core/user/action/detail.js
@@ -1,73 +1,41 @@
 module.exports = function(controller, request, response) {
-	var sequence = controller.eden.load('sequence');
-	var c = function() {
-		this.render.call();
-	}, public = c.prototype;
-	/* Loader
-	-------------------------------*/
-	public.__load = c.load = function() {
-		if(!this.__instance) {
-			this.__instance = new c();
-		}
-		return this.__instance;
-	};
-	/* Construct
-	-------------------------------*/
-	public.render = function() {
-		sequence.then(function(next) { this.validate })
-		.then(function(next) { this.setup });
-
-		return this;
+	//if no id was set
+	if(!request.variables[0]) {
+		//setup an error response
+		response.message = JSON.stringify({ 
+			error: true, 
+			message: 'No ID set' });
+		
+		//trigger that a response has been made
+		controller.trigger('user-action-response', request, response);
+		
+		return;
 	}
-	/* Public Methods
-	-------------------------------*/
-	//1. VALIDATE: if no id was set
-	public.validate = function() {
-		if(!request.variables[0]) {
+	//2. TRIGGER 
+	controller.user().store()
+	.findOne({ 
+		_id: request.variables[0], 
+		active: true })
+	.lean()
+	.exec(function(error, user) {
+		//if there are errors
+		if(error) {
 			//setup an error response
 			response.message = JSON.stringify({ 
 				error: true, 
-				message: 'No ID set' });
+				message: error.message });
 			
 			//trigger that a response has been made
 			controller.trigger('user-action-response', request, response);
-			
 			return;
 		}
-	}
-	//2. SETUP: change the string into a native object
-	public.setup = function() {
-		controller.user().store()
-		.findOne({ 
-			_id: request.variables[0], 
-			active: true })
-		.lean()
-		.exec(function(error, user) {
-			//if there are errors
-			if(error) {
-				//setup an error response
-				response.message = JSON.stringify({ 
-					error: true, 
-					message: error.message });
-				
-				//trigger that a response has been made
-				controller.trigger('user-action-response', request, response);
-
-				return;
-			}
-			
-			//no error, then prepare the package
-			response.message = JSON.stringify({ 
-				error: false, 
-				results: user });
-			
-			//trigger that a response has been made
-			controller.trigger('user-action-response', request, response);
-		});
-	}
-	/* Private Methods
-	-------------------------------*/
-	/* Adaptor
-	-------------------------------*/
-	return c.load(); 
+		
+		//no error, then prepare the package
+		response.message = JSON.stringify({ 
+			error: false, 
+			results: user });
+		
+		//trigger that a response has been made
+		controller.trigger('user-action-response', request, response);
+	});
 };
