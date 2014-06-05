54ec2506715b67e5f07dd8d7c7f78382bfa0c9a4
denezra <den.segunial@gmail.com>
2014-06-05 09:19:16 -0700

    Convert
    Convert index.js to class function

--- api/packages/core/user/action/index.js
+++ api/packages/core/user/action/index.js
@@ -1,29 +1,47 @@
 module.exports = function(controller, request, response) {
-	//if one variable is set 
-	if(request.variables[0]) {
-		//is it an update ?
-		if(request.method.toUpperCase() == 'PUT') {
-			require('./update')(controller, request, response);
-			return;
+	var c = function() {}, public = c.prototype,
+	/* Loader
+	-------------------------------*/
+	public.__load = c.load = function() {
+		public.render();
+		return new c();
+	};	
+	/* Construct
+	-------------------------------*/
+	/* Public Methods
+	-------------------------------*/
+	public.render = function() {
+		if(request.variables[0]) {
+			//is it an update ?
+			if(request.method.toUpperCase() == 'PUT') {
+				require('./update')(controller, request, response);
+				return this;
+			}
+			
+			//is it an removal ?
+			if(request.method.toUpperCase() == 'DELETE') {
+				require('./remove')(controller, request, response);
+				return this;
+			}
+			
+			//it must be a detail
+			require('./detail')(controller, request, response);
+			return this;
 		}
 		
-		//is it an removal ?
-		if(request.method.toUpperCase() == 'DELETE') {
-			require('./remove')(controller, request, response);
-			return;
+		//is it a create ?
+		if(request.method.toUpperCase() == 'POST') {
+			require('./create')(controller, request, response);
+			return this;
 		}
 		
-		//it must be a detail
-		require('./detail')(controller, request, response);
-		return;
+		//it must be a listing
+		require('./list')(controller, request, response);
+		return this;
 	}
-	
-	//is it a create ?
-	if(request.method.toUpperCase() == 'POST') {
-		require('./create')(controller, request, response);
-		return;
-	}
-	
-	//it must be a listing
-	require('./list')(controller, request, response);
-};
+	/* Private Methods
+	-------------------------------*/
+	/* Adaptor
+	-------------------------------*/
+	return c.load(); 
+}
