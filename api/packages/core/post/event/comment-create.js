6760241a1fc156db24bcd6a796f20c65a849e032
denezra <den.segunial@gmail.com>
2014-06-06 15:49:44 -0700

    Conversion of post -> event folder

--- api/packages/core/post/event/comment-create.js
+++ api/packages/core/post/event/comment-create.js
@@ -1,19 +1,57 @@
-module.exports = function(controller, id, query) {
-	//find and update
-	controller
-		.user()
-		.store()
-		.store
-		//first find the id
-		.findByIdAndUpdate(id, { $push: { comment: query } }, function(error) {
-			//if there are errors
-			if(error) {
-				//trigger an error
-				controller.trigger('user-add-address-error', error);
-				return;
-			}
-			
-			//trigger that we are good
-			controller.trigger('user-add-address-success');
-		});
-};
+module.exports = (function() { 
+	var c = function(controller, request, query) {
+		c.prototype.__construct.call(this, controller, request, query);
+    }, public = c.prototype;
+	/* Public Properties
+    -------------------------------*/
+    public.controller  	= null;
+    public.request   	= null;
+    public.query   		= null;
+    
+	/* Private Properties
+    -------------------------------*/
+    /* Loader
+    -------------------------------*/
+    public.__load = c.load = function(controller, request, query) {
+        return new c(controller, request, query);
+    };
+    
+	/* Construct
+    -------------------------------*/
+	public.__construct = function(controller, request, query) {
+		//set request and other usefull data
+		this.controller = controller;
+		this.request  	= request;
+		this.query   	= query;
+		c.prototype.render(controller, request, query);
+		//public.render();
+	};
+
+	/* Public Methods
+    -------------------------------*/
+	public.render = function(controller, request, query) {
+		//remove
+		controller
+			.user()
+			.store()
+			.store
+			//first find the id
+			.findByIdAndUpdate(request, { $push: { comment: query } }, function(error) {
+				//if there are errors
+				if(error) {
+					//trigger an error
+					controller.trigger('user-add-address-error', error);
+					return;
+				}
+				
+				//trigger that we are good
+				controller.trigger('user-add-address-success');
+			});
+	}
+	
+	/* Private Methods
+    -------------------------------*/
+	/* Adaptor
+	-------------------------------*/
+	return c; 
+})();
--- api/packages/core/post/event/post-remove.js
+++ api/packages/core/post/event/post-remove.js
@@ -1,17 +1,53 @@
-module.exports = function(controller, id) { console.log('remove');
-	//remove
-	controller
-	.post()
-	.store()
-	.remove(id, function(error) {
-		//if there are errors
-		if(error) {
-			//trigger an error
-			controller.trigger('post-remove-error', error);
-			return;
-		}
-		
-		//trigger that we are good
-		controller.trigger('post-remove-success');
-	});
-};
+module.exports = (function() { 
+	var c = function(controller, request) {
+		c.prototype.__construct.call(this, controller, request);
+    }, public = c.prototype;
+	/* Public Properties
+    -------------------------------*/
+    public.controller  	= null;
+    public.request   	= null;
+    
+	/* Private Properties
+    -------------------------------*/
+    /* Loader
+    -------------------------------*/
+    public.__load = c.load = function(controller, request) {
+        return new c(controller, request);
+    };
+    
+	/* Construct
+    -------------------------------*/
+	public.__construct = function(controller, request) {
+		//set request and other usefull data
+		this.controller = controller;
+		this.request  	= request;
+		c.prototype.render(controller, request);
+		//public.render();
+	};
+
+	/* Public Methods
+    -------------------------------*/
+	public.render = function(controller, request) {
+		//remove
+		controller
+			.post()
+			.store()
+			.remove(request, function(error) {
+				//if there are errors
+				if(error) {
+					//trigger an error
+					controller.trigger('post-remove-error', error);
+					return;
+				}
+				
+				//trigger that we are good
+				controller.trigger('post-remove-success');
+			});
+	}
+	
+	/* Private Methods
+    -------------------------------*/
+	/* Adaptor
+	-------------------------------*/
+	return c; 
+})();
