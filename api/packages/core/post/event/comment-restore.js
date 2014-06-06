3b46c4334e3642c68318633f5b8d232ef1e69e15
destromon <jestadena@yahoo.com>
2014-06-06 22:29:01 +0800

    fixed subdocument

--- api/packages/core/post/event/comment-restore.js
+++ api/packages/core/post/event/comment-restore.js
@@ -5,7 +5,7 @@ module.exports = function(controller, id, query) {
 		.store()
 		.store
 		//first find the id
-		.findByIdAndUpdate({ _id : id, 'comments._id': id  }, { $set: { active: true } }, function(error) {
+		.findByIdAndUpdate({ 'comments._id': id  }, { $set: { 'comments.active' : true } }, function(error) {
 			//if there are errors
 			if(error) {
 				//trigger an error
