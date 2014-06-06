8613457c057f41c6471934270f77eaeec38a34f1
denezra <den.segunial@gmail.com>
2014-06-06 15:34:40 -0700

    Conversion of user -> events folder

--- api/packages/core/user/event/user-create.js
+++ api/packages/core/user/event/user-create.js
@@ -1,18 +1,53 @@
-module.exports = function(controller, query) {
-	//create the model and save
-	controller
-		.user()
-		.store()
-		.model(query)
-		.save(function(error) {
-			//if there are errors
-			if(error) {
-				//trigger an error
-				controller.trigger('user-create-error', error);
-				return;
-			}
-			
-			//trigger that we are good
-			controller.trigger('user-create-success');
-		});
-};
+module.exports = (function() { 
+	var c = function(controller, query) {
+		c.prototype.__construct.call(this, controller, query);
+    }, public = c.prototype;
+	/* Public Properties
+    -------------------------------*/
+    public.controller  	= null;
+    public.query   		= null;
+    
+	/* Private Properties
+    -------------------------------*/
+    /* Loader
+    -------------------------------*/
+    public.__load = c.load = function(controller, query) {
+        return new c(controller, query);
+    };
+    
+	/* Construct
+    -------------------------------*/
+	public.__construct = function(controller, query) {
+		//set usefull data
+		this.controller = controller;
+		this.query  	= query;
+		c.prototype.render(controller, query);
+		//public.render();
+	};
+
+	/* Public Methods
+    -------------------------------*/
+	public.render = function(controller, query) {
+		//remove
+		controller
+			.user()
+			.store()
+			.remove(query, function(error) {
+				//if there are errors
+				if(error) {
+					//trigger an error
+					controller.trigger('user-create-error', error);
+					return;
+				}
+				
+				//trigger that we are good
+				controller.trigger('user-create-success');
+			});
+	}
+	
+	/* Private Methods
+    -------------------------------*/
+	/* Adaptor
+	-------------------------------*/
+	return c; 
+})();
--- api/packages/core/user/event/user-restore.js
+++ api/packages/core/user/event/user-restore.js
@@ -1,17 +1,53 @@
-module.exports = function(controller, id) {
-	//remove
-	controller
-	.user()
-	.store()
-	.restore(id, function(error) {
-		//if there are errors
-		if(error) {
-			//trigger an error
-			controller.trigger('user-restore-error', error);
-			return;
-		}
-		
-		//trigger that we are good
-		controller.trigger('user-restore-success');
-	});
-};
+module.exports = (function() { 
+	var c = function(controller, request) {
+		c.prototype.__construct.call(this, controller, request);
+    }, public = c.prototype;
+	/* Public Properties
+    -------------------------------*/
+    public.controller  	= null;
+    public.request   	= null;
+    
+	/* Private Properties
+    -------------------------------*/
+    /* Loader
+    -------------------------------*/
+    public.__load = c.load = function(controller, request) {
+        return new c(controller, request);
+    };
+    
+	/* Construct
+    -------------------------------*/
+	public.__construct = function(controller, request) {
+		//set request and other usefull data
+		this.controller = controller;
+		this.request  	= request;
+		c.prototype.render(controller, request);
+		//public.render();
+	};
+
+	/* Public Methods
+    -------------------------------*/
+	public.render = function(controller, request) {
+		//remove
+		controller
+			.user()
+			.store()
+			.restore(request, function(error) {
+				//if there are errors
+				if(error) {
+					//trigger an error
+					controller.trigger('user-restore-error', error);
+					return;
+				}
+				
+				//trigger that we are good
+				controller.trigger('user-restore-success');
+			});
+	}
+	
+	/* Private Methods
+    -------------------------------*/
+	/* Adaptor
+	-------------------------------*/
+	return c; 
+})();
