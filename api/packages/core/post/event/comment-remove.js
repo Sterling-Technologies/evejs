71d8d5d0dc7f069e5a774f6ac684ae077c3cc6b5
denezra <den.segunial@gmail.com>
2014-06-06 20:27:46 -0700

    Reconfigure

--- api/packages/core/post/event/comment-remove.js
+++ api/packages/core/post/event/comment-remove.js
@@ -5,7 +5,7 @@ module.exports = function(controller, id, query) {
 		.store()
 		.store
 		//first find the id
-		.findByIdAndUpdate(id, { $push: { comment: query } }, function(error) {
+		.findByIdAndRemove(id, { $remove: { comment: query } }, function(error) {
 			//if there are errors
 			if(error) {
 				//trigger an error
