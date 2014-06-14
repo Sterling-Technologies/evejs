9a70cebe9ded457c0d43a0bb1ae68059c39250a0
Christian Blanquera <cblanquera@openovate.com>
2014-06-15 02:34:44 +0800

    rework

--- api/config/databases.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	shipping: {
-		type: 'mongo',
-        host: 'localhost',
-		port: 27017,
-        name: 'eve',
-        user: '',
-        pass: '' }
-};
--- api/config/settings.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	api: { host: 'api.eve.dev', port : 8082 },
-	socket:  { host: 'api.eve.dev', port : 1337 }
-};
