7c4def433a12cabf332207292bc73c716404e4fc
Ian Mark Muninio <ianmuninio@gmail.com>
2014-07-23 16:49:16 +0800

    Added nodemon server starter.

--- bin/eve.js
+++ bin/eve.js
@@ -11,239 +11,250 @@
  * eve watch server		- Watches changes in server only
  * eve generate ecommerce/product
  */
-var eden 	= require('edenjs'),
-	lint 	= require('../lib/lint'),
-	local 	= process.env.PWD || process.cwd(),
-	//NOTE: maybe find a better way to find the root folder
-	root 	= process.mainModule.filename.substr(0, process.mainModule.filename.length - '/bin/eve.js'.length),
-	action 	= process.argv[2] || 'watch';
+var eden = require('edenjs'),
+        lint = require('../lib/lint'),
+        nodemon = require('../lib/nodemon'),
+        local = process.env.PWD || process.cwd(),
+        //NOTE: maybe find a better way to find the root folder
+        root = process.mainModule.filename.substr(0, process.mainModule.filename.length - '/bin/eve.js'.length),
+        action = process.argv[2] || 'watch';
 
 require('../lib/')
-	.setRoot(root)
-	//global events
-    .listen('error', function(message) {
-		console.log('\x1b[31m%s\x1b[0m', message);
-	})
+        .setRoot(root)
+        //global events
+        .listen('error', function(message) {
+            console.log('\x1b[31m%s\x1b[0m', message);
+        })
 
-	//control events
-	.listen('install-control-step-1', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Creating Control Folder ...');
-	})
-	.listen('install-control-step-2', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Control Files ...');
-	})
-	.listen('install-control-step-3', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Control Config ...');
-	})
-	.listen('install-control-step-4', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Control Config Locally ...');
-	})
-	.listen('install-control-step-5', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Control Packages ...');
-	})
-	.listen('install-control-complete', function() {
-		console.log('\x1b[32m%s\x1b[0m', 'Control Installation Complete!');
-	})
-	.listen('watch-control-init', function() {
-		console.log('\x1b[32m%s\x1b[0m', 'Watching for control changes ...');
-	})
-	.listen('watch-control-update', function(event, path, destination, eve, local, config, push) {
-		//we only care if something has changed
-		if (event !== 'change' && event !== 'add') {
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-			return;
-		}
+        //control events
+        .listen('install-control-step-1', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Creating Control Folder ...');
+        })
+        .listen('install-control-step-2', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Control Files ...');
+        })
+        .listen('install-control-step-3', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Control Config ...');
+        })
+        .listen('install-control-step-4', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Control Config Locally ...');
+        })
+        .listen('install-control-step-5', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Control Packages ...');
+        })
+        .listen('install-control-complete', function() {
+            console.log('\x1b[32m%s\x1b[0m', 'Control Installation Complete!');
+        })
+        .listen('watch-control-init', function() {
+            console.log('\x1b[32m%s\x1b[0m', 'Watching for control changes ...');
+        })
+        .listen('watch-control-update', function(event, path, destination, eve, local, config, push) {
+            //we only care if something has changed
+            if (event !== 'change' && event !== 'add') {
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+                return;
+            }
 
-		var file = eden('file', path);
-		
-		//we only care if this is a js file
-		if (file.getExtension() !== 'js') {
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-			return;
-		}
-		
-		//get the code
-		file.getContent(function(error, code) {
-			if (error) {
-				eve.trigger('error', 'Control: Error getting the content of file:' + path);
-				return;
-			}
-			
-			config = eden('hash').merge({
-				globals : {
-					define 		: true,
-					controller 	: true,
-					console 	: true,
-					require 	: true,
-					Handlebars 	: true
-				},
-				
-				bitwise : false,
-				strict 	: false,
-				browser : true,
-				jquery 	: true,
-				node 	: false
-			}, config.control.lint || {});
-			
-			//make sure node is false
-			config.node = false;
-			
-			//if there is an error
-			if(!lint(code.toString(), config, path)) {
-				return;
-			}
-			
-			//push it
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-		});
-	})
+            var file = eden('file', path);
 
-	//server events
-	.listen('install-server-step-1', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Creating Server Folder ...');
-	})
-	.listen('install-server-step-2', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Server Files ...');
-	})
-	.listen('install-server-step-3', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Server Config ...');
-	})
-	.listen('install-server-step-4', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Server Config Locally ...');
-	})
-	.listen('install-server-step-5', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Server Packages ...');
-	})
-	.listen('install-server-step-6', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'npm install ...');
-	})
-	.listen('install-server-complete', function() {
-		console.log('\x1b[32m%s\x1b[0m', 'Server Installation Complete!');
-	})
-	.listen('watch-server-init', function() {
-		console.log('\x1b[32m%s\x1b[0m', 'Watching for server changes ...');
-	})
-	.listen('watch-server-update', function(event, path, destination, eve, local, config, push) {
-		//we only care if something has changed
-		if (event !== 'change' && event !== 'add') {
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-			return;
-		}
+            //we only care if this is a js file
+            if (file.getExtension() !== 'js') {
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+                return;
+            }
 
-		var file = eden('file', path);
-		
-		//we only care if this is a js file
-		if (file.getExtension() !== 'js') {
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-			return;
-		}
-		
-		//get the code
-		file.getContent(function(error, code) {
-			if (error) {
-				eve.trigger('error', 'Server: Error getting the content of file:' + path);
-				return;
-			}
-			
-			config = eden('hash').merge({
-				bitwise : false,
-				strict 	: false,
-				node 	: true
-			}, config.control.lint || {});
-			
-			//make sure node is true
-			config.node = true;
-			
-			//if there is an error
-			if(!lint(code.toString(), config, path)) {
-				return;
-			}
-			
-			//push it
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-		});
-	})
+            //get the code
+            file.getContent(function(error, code) {
+                if (error) {
+                    eve.trigger('error', 'Control: Error getting the content of file:' + path);
+                    return;
+                }
 
-	//web events
-	.listen('install-web-step-1', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Creating Web Folder ...');
-	})
-	.listen('install-web-step-2', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Web Files ...');
-	})
-	.listen('install-web-step-3', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Web Config ...');
-	})
-	.listen('install-web-step-4', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Web Config Locally ...');
-	})
-	.listen('install-web-step-5', function() {
-		console.log('\x1b[33m%s\x1b[0m', 'Copying Web Packages ...');
-	})
-	.listen('install-web-complete', function() {
-		console.log('\x1b[32m%s\x1b[0m', 'Web Installation Complete!');
-	})
-	.listen('watch-web-init', function() {
-		console.log('\x1b[32m%s\x1b[0m', 'Watching for web changes ...');
-	})
-	.listen('watch-web-update', function(event, path, destination, eve, local, config, push) {
-		//we only care if something has changed
-		if (event !== 'change' && event !== 'add') {
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-			return;
-		}
+                config = eden('hash').merge({
+                    globals : {
+                        define : true,
+                        controller : true,
+                        console : true,
+                        require : true,
+                        Handlebars : true
+                    },
+                    bitwise : false,
+                    strict : false,
+                    browser : true,
+                    jquery : true,
+                    node : false
+                }, config.control.lint || { });
 
-		var file = eden('file', path);
-		
-		//we only care if this is a js file
-		if (file.getExtension() !== 'js') {
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-			return;
-		}
-		
-		//get the code
-		file.getContent(function(error, code) {
-			if (error) {
-				eve.trigger('error', 'Web: Error getting the content of file:' + path);
-				return;
-			}
-			
-			config = eden('hash').merge({
-				globals : {
-					define 		: true,
-					controller 	: true,
-					console 	: true,
-					require 	: true,
-					Handlebars 	: true
-				},
-				
-				bitwise : false,
-				strict 	: false,
-				browser : true,
-				jquery 	: true,
-				node 	: false
-			}, config.control.lint || {});
-			
-			//make sure node is false
-			config.node = false;
-			
-			//if there is an error
-			if(!lint(code.toString(), config, path)) {
-				return;
-			}
-			
-			//push it
-			console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
-			push(event, path, destination);
-		});
-	})
-	
-	.run(local, action);
+                //make sure node is false
+                config.node = false;
+
+                //if there is an error
+                if (!lint(code.toString(), config, path)) {
+                    return;
+                }
+
+                //push it
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+            });
+        })
+
+        //server events
+        .listen('install-server-step-1', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Creating Server Folder ...');
+        })
+        .listen('install-server-step-2', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Server Files ...');
+        })
+        .listen('install-server-step-3', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Server Config ...');
+        })
+        .listen('install-server-step-4', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Server Config Locally ...');
+        })
+        .listen('install-server-step-5', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Server Packages ...');
+        })
+        .listen('install-server-step-6', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'npm install ...');
+        })
+        .listen('install-server-complete', function() {
+            console.log('\x1b[32m%s\x1b[0m', 'Server Installation Complete!');
+        })
+        .listen('watch-server-init', function(eve, local, config) {
+            console.log('\x1b[32m%s\x1b[0m', 'Watching for server changes ...');
+
+            // build the settings
+            var settings = config.server.nodemon || { };
+
+            // if settings.cwd is not defined
+            if (!settings.cwd) {
+                // we use the default cwd is the server path
+                settings.cwd = config.server.path;
+            }
+
+            // starts the nodemon
+            nodemon(settings);
+        })
+        .listen('watch-server-update', function(event, path, destination, eve, local, config, push) {
+            //we only care if something has changed
+            if (event !== 'change' && event !== 'add') {
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+                return;
+            }
+
+            var file = eden('file', path);
+
+            //we only care if this is a js file
+            if (file.getExtension() !== 'js') {
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+                return;
+            }
+
+            //get the code
+            file.getContent(function(error, code) {
+                if (error) {
+                    eve.trigger('error', 'Server: Error getting the content of file:' + path);
+                    return;
+                }
+
+                config = eden('hash').merge({
+                    bitwise : false,
+                    strict : false,
+                    node : true
+                }, config.control.lint || { });
+
+                //make sure node is true
+                config.node = true;
+
+                //if there is an error
+                if (!lint(code.toString(), config, path)) {
+                    return;
+                }
+
+                //push it
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+            });
+        })
+
+        //web events
+        .listen('install-web-step-1', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Creating Web Folder ...');
+        })
+        .listen('install-web-step-2', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Web Files ...');
+        })
+        .listen('install-web-step-3', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Web Config ...');
+        })
+        .listen('install-web-step-4', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Web Config Locally ...');
+        })
+        .listen('install-web-step-5', function() {
+            console.log('\x1b[33m%s\x1b[0m', 'Copying Web Packages ...');
+        })
+        .listen('install-web-complete', function() {
+            console.log('\x1b[32m%s\x1b[0m', 'Web Installation Complete!');
+        })
+        .listen('watch-web-init', function() {
+            console.log('\x1b[32m%s\x1b[0m', 'Watching for web changes ...');
+        })
+        .listen('watch-web-update', function(event, path, destination, eve, local, config, push) {
+            //we only care if something has changed
+            if (event !== 'change' && event !== 'add') {
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+                return;
+            }
+
+            var file = eden('file', path);
+
+            //we only care if this is a js file
+            if (file.getExtension() !== 'js') {
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+                return;
+            }
+
+            //get the code
+            file.getContent(function(error, code) {
+                if (error) {
+                    eve.trigger('error', 'Web: Error getting the content of file:' + path);
+                    return;
+                }
+
+                config = eden('hash').merge({
+                    globals : {
+                        define : true,
+                        controller : true,
+                        console : true,
+                        require : true,
+                        Handlebars : true
+                    },
+                    bitwise : false,
+                    strict : false,
+                    browser : true,
+                    jquery : true,
+                    node : false
+                }, config.control.lint || { });
+
+                //make sure node is false
+                config.node = false;
+
+                //if there is an error
+                if (!lint(code.toString(), config, path)) {
+                    return;
+                }
+
+                //push it
+                console.log('\x1b[32m%s\x1b[0m', event + ' - ' + destination);
+                push(event, path, destination);
+            });
+        })
+
+        .run(local, action);
