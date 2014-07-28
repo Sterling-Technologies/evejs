a03220d500d07b4d6b8f93202886563a545ccd8a
Charles Zamora <charles.andacc@gmail.com>
2014-07-28 18:13:43 +0800

    update address and address-user copy

--- package/core/address/server/event/address-create.js
+++ package/core/address/server/event/address-create.js
@@ -1,10 +1,10 @@
 module.exports = function(controller, query) {
 	//create the model and save
-	controller.address().store().insert(query, function(error) {
+	controller.address().store().insert(query, function(error, results) {
 		if(error) {
 			return controller.trigger('address-create-error', error);
 		}
 
-		return controller.trigger('address-create-success');
+		return controller.trigger('address-create-success', results);
 	});
 };
